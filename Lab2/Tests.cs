f03300922f20a9936339d6d649da1b483edc8349
MishaGontar <turictscol40@gmail.com>
2023-11-25 22:36:59 +0200

    update project and prepare application to lab 2

---
+++ Lab2/Tests.cs
@@ -0,0 +1,16 @@
+﻿using ConsoleApplication1.lab1.model;
+using NUnit.Framework;
+
+namespace Lab2
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Node<bool> n = new Node<bool>(true);
+            Assert.True(n.Data);
+        }
+    }
+}
