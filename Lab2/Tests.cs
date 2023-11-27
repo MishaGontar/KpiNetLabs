e9ef6cfa61935d095c903e387d5e6d54be08152e
MishaGontar <turictscol40@gmail.com>
2023-11-27 18:48:32 +0200

    finish lab 2

--- Lab2/Tests.cs
+++ Lab2/Tests.cs
@@ -1,16 +1,61 @@
-﻿using ConsoleApplication1.lab1.model;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using ConsoleApplication1.lab1;
 
-namespace Lab2
+[TestFixture]
+public class QueueTests
 {
-    [TestFixture]
-    public class Tests
+    private Queue<string> _queue;
+
+    [SetUp]
+    public void SetUp()
     {
-        [Test]
-        public void Test1()
-        {
-            Node<bool> n = new Node<bool>(true);
-            Assert.True(n.Data);
-        }
+        _queue = new Queue<string>();
+    }
+
+    [Test]
+    public void Enqueue_WhenCalled_ShouldAddItemToQueue()
+    {
+        _queue.Enqueue("test");
+
+        Assert.That(_queue.Count(), Is.EqualTo(1));
+    }
+
+    [Test]
+    public void Dequeue_WhenCalled_ShouldRemoveItemFromQueue()
+    {
+        _queue.Enqueue("test");
+        _queue.Dequeue();
+
+        Assert.That(_queue.Count(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Dequeue_WhenQueueIsEmpty_ShouldThrowInvalidOperationException()
+    {
+        Assert.Throws<InvalidOperationException>(() => _queue.Dequeue());
+    }
+
+    [Test]
+    public void Clear_WhenCalled_ShouldRemoveAllItemsFromQueue()
+    {
+        _queue.Enqueue("test1");
+        _queue.Enqueue("test2");
+        _queue.Clear();
+
+        Assert.That(_queue.Count(), Is.EqualTo(0));
+    }
+
+    [Test]
+    public void Enqueue_WhenDataIsNull_ShouldThrowArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => _queue.Enqueue(null));
+    }
+
+    [Test]
+    public void AddToFront_WhenDataIsNull_ShouldThrowArgumentNullException()
+    {
+        Assert.Throws<ArgumentNullException>(() => _queue.AddToFront(null));
     }
 }
